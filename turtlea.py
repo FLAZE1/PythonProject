2feda617d0bb5bd1c3426ac3eb7b4a04c9ac3c19
Flaze <miruonlymir@gmail.com>
2026-03-31 12:26:15 +0200

    smth new

--- turtlea.py
+++ turtlea.py
@@ -83,39 +83,200 @@ from turtle import*
 # done()
 
 
+# m=10
+# tracer(False)
+# screensize(5000,5000)
+# left(90)
+#
+# for i in range(3):
+#     forward(7*m)
+#     right(90)
+#     forward(12*m)
+#     right(90)
+#
+# up()
+#
+# forward(4*m)
+# right(90)
+# forward(6*m)
+# left(90)
+#
+# down()
+#
+# for i in range(4):
+#     forward(83*m)
+#     right(90)
+#     forward(77*m)
+#     right(90)
+# up()
+#
+# for x in range(-71,13):
+#     for y in range(-80,8):
+#         goto(x*m,y*m)
+#         dot(3,'red')
+#
+#
+#
+#
+# update()
+# done()
+
+
+# m=10
+# tracer(False)
+# screensize(5000,5000)
+# left(90)
+#
+# for i in range(4):
+#     forward(19*m)
+#     right(90)
+#     forward(30*m)
+#     right(90)
+#
+# up()
+#
+# forward(2*m)
+# right(90)
+# forward(8*m)
+# left(90)
+#
+# down()
+#
+# for i in range(4):
+#     forward(93*m)
+#     right(90)
+#     forward(97*m)
+#     right(90)
+# up()
+#
+# for x in range(8,31):
+#     for y in range(2,20):
+#         goto(x*m,y*m)
+#         dot(3,'red')
+#
+#
+#
+#
+# update()
+# done()
+#
+# print(23*18)
+
+
+# m=20
+# tracer(False)
+# screensize(5000,5000)
+# left(90)
+#
+# for i in range(9):
+#     forward(22*m)
+#     right(90)
+#     forward(6*m)
+#     right(90)
+#
+# up()
+#
+# forward(1*m)
+# right(90)
+# forward(5*m)
+# left(90)
+#
+# down()
+#
+# for i in range(9):
+#     forward(53*m)
+#     right(90)
+#     forward(75*m)
+#     right(90)
+# up()
+#
+# for x in range(0,2):
+#     for y in range(0,22):
+#         goto(x*m,y*m)
+#         dot(3,'red')
+#
+#
+#
+#
+# update()
+# done()
+
+
+# m=10
+# tracer(False)
+# screensize(5000,5000)
+# left(90)
+#
+# for i in range(2):
+#     forward(5*m)
+#     left(90)
+#     back(13*m)
+#     left(90)
+#
+# up()
+#
+# back(10*m)
+# right(90)
+# forward(9*m)
+# left(90)
+#
+# down()
+#
+# for i in range(2):
+#     forward(11*m)
+#     right(90)
+#     forward(7*m)
+#     right(90)
+# up()
+#
+# for x in range(0,17):
+#     for y in range(-10,6):
+#         goto(x*m,y*m)
+#         dot(3,'red')
+#
+# print(14*6+8*12-10)
+#
+#
+#
+#
+# update()
+# done()
+
+
+
 m=10
 tracer(False)
 screensize(5000,5000)
 left(90)
 
-for i in range(3):
-    forward(7*m)
-    right(90)
-    forward(12*m)
-    right(90)
+for i in range(2):
+    forward(23*m)
+    left(90)
+    back(27*m)
+    left(90)
 
 up()
 
-forward(4*m)
+back(5*m)
 right(90)
-forward(6*m)
+forward(11*m)
 left(90)
 
 down()
 
-for i in range(4):
-    forward(83*m)
+for i in range(2):
+    forward(26*m)
     right(90)
-    forward(77*m)
+    forward(32*m)
     right(90)
 up()
 
-for x in range(-71,13):
-    for y in range(-80,8):
+for x in range(11,28):
+    for y in range(0,22):
         goto(x*m,y*m)
         dot(3,'red')
 
-
+print(24*28+27*33-17*22)
 
 
 update()
