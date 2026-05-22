ea1f18166ed16f16b1df9e74c8bf7736b9191c51
flaze <miruonlymir@gmail.com>
2026-05-22 19:17:17 +0200

    smth new

--- 16_4.py
+++ 16_4.py
@@ -266,12 +266,13 @@ print(num_sum)
 #         mini=b
 # print(mini)
 
-s=0
+# s=0
+#
+# while True:
+#     a=int(input())
+#     if a==0:
+#         break
+#     if a % 4 == 0 or a % 9 == 0:
+#         s+=a
+# print(s)
 
-while True:
-    a=int(input())
-    if a==0:
-        break
-    if a % 4 == 0 or a % 9 == 0:
-        s+=a
-print(s)
